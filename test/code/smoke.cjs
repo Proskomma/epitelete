34d64049bb7c6fe7bf1d48f362b6881d7a2bf262
Abel Perez <abelper54@gmail.com>
2022-05-21 09:32:10 -0400

    Add tests for fetchPerf method

--- test/code/smoke.cjs
+++ test/code/smoke.cjs
@@ -13,6 +13,8 @@ const pk = new UWProskomma();
 const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, '..', 'test_data', 'eng_engWEBBE_succinct.json')));
 pk.loadSuccinctDocSet(succinctJson);
 
+const epitelete = new Epitelete(pk, "DBL/eng_engWEBBE");
+
 test(
     `Instantiate Epitelete (${testGroup})`,
     async function (t) {
@@ -20,7 +22,7 @@ test(
             t.plan(3);
             t.doesNotThrow(() => new Epitelete(pk, "DBL/eng_engWEBBE"));
             t.throws(() => new Epitelete(pk), "2 arguments");
-            t.throws(() => new Epitelete(pk, "eBible/fra_fraLSG"),"docSetId not found");
+            t.throws(() => new Epitelete(pk, "eBible/fra_fraLSG"),"docSetId is not present");
         } catch (err) {
             console.log(err);
         }
@@ -28,43 +30,27 @@ test(
 );
 
 test(
-    `Hello - REPLACE ASAP (${testGroup})`,
+    `Run fetchPerf (${testGroup})`,
     async function (t) {
         try {
-            t.plan(1);
-            const config = {
-                selectedSequenceId: null,
-                allSequences: true,
-                output: {
-                    docSets: {},
-                }
-            };
-            const query = '{docSets { id documents {id bookCode: header(id:"bookCode")} } }';
-            const gqlResult = pk.gqlQuerySync(query);
-            const docSetId = gqlResult.data.docSets[0].id;
-            const bookCode = "LUK";
-            const documentId = gqlResult.data.docSets[0].documents.filter(d => d.bookCode === bookCode)[0].id;
-            const config2 = await doRender(
-                pk,
-                config,
-                [docSetId],
-                [documentId],
-            );
-            // console.log(config2.output.docSets[docSetId].documents[bookCode].sequences);
-            t.equal(config2.validationErrors, null);
-            const ret = {
-                docSetId,
-                documentId,
-                mainSequenceId: config2.output.docSets[docSetId].documents[bookCode].mainSequence,
-                headers: config2.output.docSets[docSetId].documents[bookCode].headers,
-                sequenceHtml: {},
-            };
-            Object.keys(config2.output.docSets[docSetId].documents[bookCode].sequences)
-                .forEach(seqId => {ret.sequenceHtml[seqId] = perf2html(config2.output, seqId)});
-            // console.log(JSON.stringify(ret, null, 2));
-            // console.log(perf2html(config2.output));
-            // console.log(JSON.stringify(config2.output.docSets[docSetId].documents[bookCode]));
-            // console.log(config2.validationErrors);
+            t.plan(3);
+            t.doesNotThrow(() => epitelete.fetchPerf("LUK"));
+            t.throws(() => epitelete.fetchPerf(), "1 argument");
+            t.throws(() => epitelete.fetchPerf("DNE"), "No document");
+
+            // const ret = {
+            //     docSetId,
+            //     documentId,
+            //     mainSequenceId: config2.output.docSets[docSetId].documents[bookCode].mainSequence,
+            //     headers: config2.output.docSets[docSetId].documents[bookCode].headers,
+            //     sequenceHtml: {},
+            // };
+            // Object.keys(config2.output.docSets[docSetId].documents[bookCode].sequences)
+            //     .forEach(seqId => {ret.sequenceHtml[seqId] = perf2html(config2.output, seqId)});
+            // // console.log(JSON.stringify(ret, null, 2));
+            // // console.log(perf2html(config2.output));
+            // // console.log(JSON.stringify(config2.output.docSets[docSetId].documents[bookCode]));
+            // // console.log(config2.validationErrors);
         } catch (err) {
             console.log(err);
         }

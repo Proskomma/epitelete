84d88ceef9a65ff0bfed8221436628c3a86da94a
Abel Perez <abelper54@gmail.com>
2022-05-21 09:31:40 -0400

    Implemented fetchPerf method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const {doRender} = require('proskomma-render-perf');
 class Epitelete {
 
     constructor(pk, docSetId) {
@@ -5,15 +6,50 @@ class Epitelete {
             throw new Error("Epitelete constructor requires 2 arguments (pk, docSetId)");
         }
 
-        const query = `{ docSets(ids: "${docSetId}") { id } }`;
-        const gqlResult = pk.gqlQuerySync(query);
+        const query = `{ docSet(id: "${docSetId}") { id } }`;
+        const { data: gqlResult } = pk.gqlQuerySync(query);
 
-        if (!gqlResult.data.docSets.length) {
+        if (!gqlResult.docSet) {
             throw new Error("Provided docSetId is not present in the Proskomma instance.");
         }
 
         this.pk = pk;
         this.docSetId = docSetId;
+    }
+
+    async fetchPerf(bookCode) {
+
+        if (!bookCode) {
+            throw new Error("fetchPerf requires 1 argument (bookCode)");
+        }
+
+        const config = {
+            selectedSequenceId: null,
+            allSequences: true,
+            output: {
+                docSets: {},
+            }
+        };
+        const query = `{docSet(id: "${this.docSetId}") { id document(bookCode: "${bookCode}") {id bookCode: header(id:"bookCode")} } }`;
+        const { data: { docSet } } = this.pk.gqlQuerySync(query);
+        const documentId = docSet.document?.id;
+
+        if (!documentId) {
+            throw new Error(`No document with bookCode="${bookCode}" found.`);
+        }
+
+        const config2 = await doRender(
+            this.pk,
+            config,
+            [this.docSetId],
+            [documentId],
+        );
+
+        if (config2.validationErrors) {
+            throw new Error(`doRender validation error`);
+        }
+
+        return config2.output;
     }
 }
 

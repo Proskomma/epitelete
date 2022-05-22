bcbe5b6699205373943b94fc09e07193910ff7eb
Abel Perez <abelper54@gmail.com>
2022-05-22 12:58:50 -0400

    added tests for issue 5

--- test/code/smoke.cjs
+++ test/code/smoke.cjs
@@ -27,38 +27,185 @@ test(
     },
 );
 
-test(
-    `Call fetchPerf (${testGroup})`,
-    async function (t) {
-        t.plan(8);
-        try{
-            const docSetId = "DBL/eng_engWEBBE";
-            const bookCode = "LUK";
-            const epitelete = new Epitelete(pk, docSetId);
-            const output = await epitelete.fetchPerf(bookCode);
-            t.ok("docSets" in output);
-            t.ok(docSetId in output.docSets);
-            t.ok("documents" in output.docSets[docSetId]);
-            t.ok(bookCode in output.docSets[docSetId].documents);
-            const doc = output.docSets[docSetId].documents[bookCode];
-            for (const k of ['headers', 'tags', 'sequences', 'mainSequence']) {
-                t.ok(k in doc);
-            }
+// fetchPerf tests
 
-            // Make HTML - move to subclass!
-            const ret = {
-                     docSetId,
-                     mainSequenceId: output.docSets[docSetId].documents[bookCode].mainSequence,
-                     headers: output.docSets[docSetId].documents[bookCode].headers,
-                     sequenceHtml: {},
-                 };
-            Object.keys(output.docSets[docSetId].documents[bookCode].sequences)
-                .forEach(seqId => {ret.sequenceHtml[seqId] = perf2html(output, seqId)});
-            // console.log(JSON.stringify(ret, null, 2));
+test(
+    `fetchPerf method (${testGroup})`,
+    async t => {
+
+        const docSetId = "DBL/eng_engWEBBE";
+        const epitelete = new Epitelete(pk, docSetId);
+        
+        t.test(
+            "fetchPerf() is defined",
+            async t => {
+                t.ok(typeof epitelete.fetchPerf === 'function');
+                t.end();
+            }
+        )
             
-        } catch (err) {
-            t.error(err);
-            console.log(err);
-        }
-    },
-);
+        t.test(
+            `fetchPerf() returns config output`,
+            async t => {
+                try {
+                    const bookCode = "LUK";
+                    const output = await epitelete.fetchPerf(bookCode);
+                    t.ok("docSets" in output);
+                    t.ok(docSetId in output.docSets);
+                    t.ok("documents" in output.docSets[docSetId]);
+                    t.ok(bookCode in output.docSets[docSetId].documents);
+                    const doc = output.docSets[docSetId].documents[bookCode];
+                    for (const k of ['headers', 'tags', 'sequences', 'mainSequence']) {
+                        t.ok(k in doc);
+                    }
+                    // Make HTML - move to subclass!
+                    const ret = {
+                        docSetId,
+                        mainSequenceId: output.docSets[docSetId].documents[bookCode].mainSequence,
+                        headers: output.docSets[docSetId].documents[bookCode].headers,
+                        sequenceHtml: {},
+                    };
+                    Object.keys(output.docSets[docSetId].documents[bookCode].sequences)
+                        .forEach(seqId => { ret.sequenceHtml[seqId] = perf2html(output, seqId) });
+                    // console.log(JSON.stringify(ret, null, 2));
+                    
+                } catch (err) {
+                    t.error(err)
+                    console.log(err);
+                }
+                t.end()
+            }
+        )
+
+        t.test(
+            `fetchPerf() adds document to documents property`,
+            async t => {
+                t.plan(2);
+                const bookCode = "MRK";
+                t.notOk(bookCode in epitelete.documents);
+                await epitelete.fetchPerf(bookCode);
+                t.ok(bookCode in epitelete.documents)
+            }
+        )
+    }
+)
+
+test(
+    `readPerf method (${testGroup})`,
+    async t => {
+
+        const docSetId = "DBL/eng_engWEBBE";
+        const epitelete = new Epitelete(pk, docSetId);
+        
+        t.test(
+            "readPerf() is defined",
+            async t => {
+                t.ok(typeof epitelete.readPerf === 'function');
+                t.end();
+            }
+        )
+            
+        t.test(
+            `readPerf returns config output`,
+            async t => {
+                try {
+                    const bookCode = "LUK";
+                    const output = await epitelete.readPerf(bookCode);
+                    t.ok("docSets" in output);
+                    t.ok(docSetId in output.docSets);
+                    t.ok("documents" in output.docSets[docSetId]);
+                    t.ok(bookCode in output.docSets[docSetId].documents);
+                    const doc = output.docSets[docSetId].documents[bookCode];
+                    for (const k of ['headers', 'tags', 'sequences', 'mainSequence']) {
+                        t.ok(k in doc);
+                    }
+                    // Make HTML - move to subclass!
+                    const ret = {
+                        docSetId,
+                        mainSequenceId: output.docSets[docSetId].documents[bookCode].mainSequence,
+                        headers: output.docSets[docSetId].documents[bookCode].headers,
+                        sequenceHtml: {},
+                    };
+                    Object.keys(output.docSets[docSetId].documents[bookCode].sequences)
+                        .forEach(seqId => { ret.sequenceHtml[seqId] = perf2html(output, seqId) });
+                    // console.log(JSON.stringify(ret, null, 2));
+                    
+                } catch (err) {
+                    t.error(err);
+                    console.log(err);
+                }
+                t.end()
+            }
+        )
+    }
+)
+
+test(
+    `localBookCodes method (${testGroup})`,
+    async t => {
+
+        const docSetId = "DBL/eng_engWEBBE";
+        const epitelete = new Epitelete(pk, docSetId);
+        
+        t.test(
+            "localBookCodes() is defined",
+            async t => {
+                t.ok(typeof epitelete.localBookCodes === 'function');
+                t.end();
+            }
+        )
+            
+        t.test(
+            `localBookCodes returns list of document keys`,
+            async t => {
+                try {
+                    const bookCodes = ["LUK", "MRK", "3JN", "GEN"];
+                    for (const bookCode of bookCodes) {
+                        await epitelete.readPerf(bookCode);
+                    }
+                    const localBookCodes = epitelete.localBookCodes();
+                    t.ok(localBookCodes)
+                    t.deepEqual(localBookCodes, bookCodes);
+                } catch (err) {
+                    t.error(err);
+                    console.log(err);
+                }
+                t.end()
+            }
+        )
+    }
+)
+
+test(
+    `clearPerf method (${testGroup})`,
+    async t => {
+
+        const docSetId = "DBL/eng_engWEBBE";
+        const epitelete = new Epitelete(pk, docSetId);
+        
+        t.test(
+            "clearPerf() is defined",
+            async t => {
+                t.ok(typeof epitelete.clearPerf === 'function');
+                t.end();
+            }
+        )
+            
+        t.test(
+            `clearPerf clears list of document keys`,
+            async t => {
+                try {
+                    const bookCode = "LUK";
+                    await epitelete.readPerf(bookCode);
+                    t.ok("LUK" in epitelete.documents);
+                    epitelete.clearPerf()
+                    t.same(epitelete.documents, {});
+                } catch (err) {
+                    t.error(err);
+                    console.log(err);
+                }
+                t.end()
+            }
+        )
+    }
+)

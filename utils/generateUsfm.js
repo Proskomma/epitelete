2e2cf3aaa37abe2f9cbeac5823449f48d2936938
Abel Perez <abelper54@gmail.com>
2022-07-19 13:37:44 -0400

    added generateUsfm

---
+++ utils/generateUsfm.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const fse = require("fs-extra");
+const {UWProskomma} = require("uw-proskomma");
+const Epitelete = require("../src/index").default;
+
+const args = process.argv.slice(3)
+const pk = new UWProskomma();
+
+console.log("Loading source to proskomma...");
+const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, "..", "test", "test_data", "fra_lsg_succinct.json")));
+const docSetId = "eBible/fra_fraLSG";
+// const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, "..", "test", "test_data", "eng_engWEBBE_succinct.json")));
+// const docSetId = "DBL/eng_engWEBBE";
+pk.loadSuccinctDocSet(succinctJson);
+
+export const generatePerf = async () => {
+  const instance = new Epitelete({proskomma: pk, docSetId});
+  const bookCode = (args?.[0] || "psa").toUpperCase();
+  if (! args[0]) 
+    console.log("\u001B[33m", `No book code provided, generating USFM for ${bookCode} instead...`, "\u001B[0m")
+   else 
+    console.log(`Loading ${bookCode} document...`);
+  
+  const usfm = await instance.readUsfm(bookCode);
+  console.log("Generating USFM file...");
+  const dir = path.resolve(__dirname, "..", "test", "test_data", "generated");
+  if (! fse.pathExistsSync(dir)) 
+    fse.mkdirSync(dir);
+  
+  const safeDocSetId = instance.docSetId.replace("/", "-");
+  const fileName = `${bookCode}-${safeDocSetId}.usfm`;
+  const filePath = path.resolve(dir, fileName);
+  fse.writeFileSync(filePath, usfm, {encoding: 'utf8'});
+  console.log("\u001B[32m", `✔ JSON file generated for ${bookCode} at ${filePath}`, "\u001B[0m");
+};
+
+if (process?.argv?.length) generatePerf();

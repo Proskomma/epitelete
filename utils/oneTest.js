fa978726e3d6909f405632a39481b9f5e41ded93
Abel Perez <abelper54@gmail.com>
2022-11-02 23:27:42 -0400

    added test utility script

---
+++ utils/oneTest.js
@@ -0,0 +1,38 @@
+const { spawn } = require("node:child_process");
+const path = require("path");
+
+try {
+  const [, , customTarget] = process.argv;
+  const DEFAULT_TARGET = "standalone";
+  const target = customTarget || DEFAULT_TARGET;
+  const targetPath = path.resolve(__dirname, "..", "test", "code", `${target}.cjs`);
+  const command = spawn(
+    "tape",
+    [
+      `-r @babel/register`,
+      targetPath,
+    ],
+    { shell: true }
+  );
+  console.log(
+    "\x1b[32m",
+    `✏️ ~ Executing command: ${command.spawnargs.at(-1)}...`,
+    "\x1b[0m"
+  );
+  console.log(
+    "\x1b[32m",
+    `⚙️ ~ Running tests on: "${target}"...`,
+    "\x1b[0m"
+  );
+  command.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  command.stdout.on("data", (output) => {
+    console.log(output.toString());
+  });
+  command.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+} catch (error) {
+  console.error(error);
+}

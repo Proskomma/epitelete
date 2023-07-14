a0d17fdc132945dd62ae39f23fdf6df4615071c4
danielc-n <npmdanielc-n@protonmail.com>
2023-07-14 09:10:30 +0200

    added usfm to aligned perf transform

--- src/pipelines/index.js
+++ src/pipelines/index.js
@@ -1,7 +1,9 @@
 import reports from "./reports";
+import alignedUsfmToAlignedPerf from './alignedUsfmToAlignedPerf.json'
 
 const wordSearch = reports.wordSearch;
 
 export default {
     wordSearch,
+    alignedUsfmToAlignedPerf
 }
--- src/transforms/index.js
+++ src/transforms/index.js
@@ -1,5 +1,9 @@
 import wordSearch from "./wordSearch";
+import perfToAlignedPerfFormat from "./perfToAlignedPerfFormat";
+import stripAlignment from "./stripAlignment";
 
 export default {
     wordSearch,
+    perfToAlignedPerfFormat,
+    stripAlignment
 }
